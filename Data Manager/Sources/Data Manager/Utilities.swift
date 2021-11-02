974a85a668300919b093e2e3c364ab231b4e1c0b
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2021-11-02 16:46:09 -0400

    Add backwards-compatible `session.data(from:)` implementation

--- Data Manager/Sources/Data Manager/Utilities.swift
+++ Data Manager/Sources/Data Manager/Utilities.swift
@@ -44,4 +44,21 @@ extension Date {
 	}
 	
 }
+
+extension URLSession {
+	
+	func data(from url: URL) async throws -> (Data, URLResponse) {
+		try await withCheckedThrowingContinuation { (continuation) in
+			let task = self.dataTask(with: url) { (data, response, error) in
+				guard let data = data, let response = response else {
+					let error = error ?? URLError(.badServerResponse)
+					return continuation.resume(throwing: error)
+				}
+				continuation.resume(returning: (data, response))
+			}
+			task.resume()
+		}
+	}
+	
+}
 #endif // os(Linux)
