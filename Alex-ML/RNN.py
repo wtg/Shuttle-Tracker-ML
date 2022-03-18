cd26650a272117f3050b52b86f76f1e9ce8feeaa
Alex <xiejialin88@gmail.com>
2022-03-18 16:11:35 -0400

    RNN
    fixed small issues

--- Alex-ML/RNN.py
+++ Alex-ML/RNN.py
@@ -2,7 +2,7 @@ from gc import callbacks
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import os
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import *
@@ -14,7 +14,7 @@ from tensorflow.keras.optimizers import Adam
 
 df = pd.read_csv("Data_session2.csv")
 
-df = df[:79]
+
 
 latitude = df["Latitude"]
 longitude = df["Longitude"]
@@ -30,13 +30,15 @@ def df_to_X_y(df, session):
         y.append(label)
     return np.array(X), np.array(y)
 
+print("LATITUDE MODEL \n")
+
 SESSION = 5
 X, y = df_to_X_y(latitude, SESSION)
 print(X.shape, y.shape)
 
-X_train, y_train = X[44:1000], y[44:1000]
-X_val, y_val = X[1000:1482], y[1000:1482]
-X_test, y_test = X[:44], y[:44]
+X_train, y_train = X[:2000], y[:2000]
+X_val, y_val = X[2000:4000], y[2000:4000]
+X_test, y_test = X[4000:], y[4000:]
 
 print(X_train.shape, y_train.shape)
 print(X_val.shape, y_val.shape)
@@ -44,23 +46,25 @@ print(X_test.shape, y_test.shape)
 
 model1 = Sequential()
 model1.add(InputLayer((5,1)))
-model1.add(LSTM(64))
-model1.add(Dense(8, 'relu'))
-model1.add(Dense(1, 'linear'))
+model1.add(LSTM(100))
+model1.add(Dense(8, activation = 'relu'))
+model1.add(Dense(1, activation = 'linear'))
 
 model1.summary()
 
 cp = ModelCheckpoint('model1/', save_best_only = True)
-model1.compile(loss = MeanSquaredError(), optimizer = Adam(learning_rate = 0.1), metrics = [RootMeanSquaredError()])
+model1.compile(loss = MeanSquaredError(), optimizer = Adam(learning_rate = 0.001), metrics = [RootMeanSquaredError()])
 
-model1.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 10, callbacks = [cp])
+model1.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 5, callbacks = [cp])
+
+print("LONGITUDE MODEL \n")
 
 X, y = df_to_X_y(longitude, SESSION)
 print(X.shape, y.shape)
 
-X_train, y_train = X[44:1000], y[44:1000]
-X_val, y_val = X[1000:1482], y[1000:1482]
-X_test, y_test = X[:44], y[:44]
+X_train, y_train = X[:2000], y[:2000]
+X_val, y_val = X[2000:4000], y[2000:4000]
+X_test, y_test = X[4000:], y[4000:]
 
 print(X_train.shape, y_train.shape)
 print(X_val.shape, y_val.shape)
@@ -68,14 +72,14 @@ print(X_test.shape, y_test.shape)
 
 model2 = Sequential()
 model2.add(InputLayer((5,1)))
-model2.add(LSTM(64))
+model2.add(LSTM(100))
 model2.add(Dense(8, 'relu'))
 model2.add(Dense(1, 'linear'))
 
 model2.summary()
 
 cp = ModelCheckpoint('model1/', save_best_only = True)
-model2.compile(loss = MeanSquaredError(), optimizer = Adam(learning_rate = 0.1), metrics = [RootMeanSquaredError()])
+model2.compile(loss = MeanSquaredError(), optimizer = Adam(learning_rate = 0.001), metrics = [RootMeanSquaredError()])
 
-model2.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 10, callbacks = [cp])
+model2.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 5, callbacks = [cp])
 
