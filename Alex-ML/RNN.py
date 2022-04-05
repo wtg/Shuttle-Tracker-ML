45bfcbab3adcca876afb8599db1dbe010fca61e7
Alex <xiejialin88@gmail.com>
2022-04-04 23:39:22 -0400

    rnn modification

--- Alex-ML/RNN.py
+++ Alex-ML/RNN.py
@@ -1,4 +1,5 @@
 from gc import callbacks
+from tabnanny import verbose
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -8,6 +9,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import *
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.losses import MeanAbsolutePercentageError
 from tensorflow.keras.metrics import RootMeanSquaredError
 from tensorflow.keras.optimizers import Adam
 
@@ -33,7 +35,7 @@ def df_to_X_y(df, session):
 print("LATITUDE MODEL \n")
 
 SESSION = 5
-X, y = df_to_X_y(latitude, SESSION)
+X, y = df_to_X_y(latitude, 5)
 print(X.shape, y.shape)
 
 X_train, y_train = X[:2000], y[:2000]
@@ -46,40 +48,48 @@ print(X_test.shape, y_test.shape)
 
 model1 = Sequential()
 model1.add(InputLayer((5,1)))
-model1.add(LSTM(100))
-model1.add(Dense(8, activation = 'relu'))
+model1.add(LSTM(100,recurrent_dropout= 0.1))
+model1.add(Dense(5, activation = 'relu'))
 model1.add(Dense(1, activation = 'linear'))
 
 model1.summary()
 
 cp = ModelCheckpoint('model1/', save_best_only = True)
-model1.compile(loss = MeanSquaredError(), optimizer = Adam(learning_rate = 0.001), metrics = [RootMeanSquaredError()])
+model1.compile(loss = MeanAbsolutePercentageError(), optimizer = Adam(learning_rate = 0.001),metrics = ['accuracy'])
 
-model1.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 5, callbacks = [cp])
+model1.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 10, callbacks = [cp])
 
-print("LONGITUDE MODEL \n")
+score = model1.evaluate(X_test, y_test)
 
-X, y = df_to_X_y(longitude, SESSION)
-print(X.shape, y.shape)
+print('test loss', score)
+#print('accuracy', score[1])
 
-X_train, y_train = X[:2000], y[:2000]
-X_val, y_val = X[2000:4000], y[2000:4000]
-X_test, y_test = X[4000:], y[4000:]
+# print("LONGITUDE MODEL \n")
 
-print(X_train.shape, y_train.shape)
-print(X_val.shape, y_val.shape)
-print(X_test.shape, y_test.shape)
+# X, y = df_to_X_y(longitude, SESSION)
+# print(X.shape, y.shape)
 
-model2 = Sequential()
-model2.add(InputLayer((5,1)))
-model2.add(LSTM(100))
-model2.add(Dense(8, 'relu'))
-model2.add(Dense(1, 'linear'))
+# X_train, y_train = X[:2000], y[:2000]
+# X_val, y_val = X[2000:4000], y[2000:4000]
+# X_test, y_test = X[4000:], y[4000:]
 
-model2.summary()
+# print(X_train.shape, y_train.shape)
+# print(X_val.shape, y_val.shape)
+# print(X_test.shape, y_test.shape)
 
-cp = ModelCheckpoint('model1/', save_best_only = True)
-model2.compile(loss = MeanSquaredError(), optimizer = Adam(learning_rate = 0.001), metrics = [RootMeanSquaredError()])
+# model2 = Sequential()
+# model2.add(InputLayer((5,1)))
+# model2.add(LSTM(100))
+# model2.add(Dense(8, 'relu'))
+# model2.add(Dense(1, 'linear'))
 
-model2.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 5, callbacks = [cp])
+# model2.summary()
 
+# cp = ModelCheckpoint('model1/', save_best_only = True)
+# model2.compile(loss = MeanSquaredError(), optimizer = Adam(learning_rate = 0.01), metrics = [RootMeanSquaredError()])
+
+# model2.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 5, callbacks = [cp])
+
+
+# print('test loss', model2.evaluate(X_test, y_test, verbose = 0)[0])
+# print('accuracy', model2.evaluate(X_test, y_test, verbose = 0)[1])
