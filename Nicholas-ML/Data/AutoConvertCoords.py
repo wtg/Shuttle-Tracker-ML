34ae31e7e63e0fb6068e6e89b3355db1c37b77c3
Busaba-Nicholas <busabaN2014@verizon.net>
2024-02-17 22:45:03 -0500

    Added Brute Force Algorithm for insurance

--- Nicholas-ML/Data/AutoConvertCoords.py
+++ Nicholas-ML/Data/AutoConvertCoords.py
@@ -1,10 +1,28 @@
 import sys
+import math
 from sklearn.neighbors import KDTree
 if (len(sys.argv) < 3):
     print("Have the command line arguments be the file you want to read from and the file you want to write to as.")
-    exit()
+    exit(1)
 fileToReadFrom = sys.argv[1]
 fileToWriteTo = sys.argv[2]
+
+# Determines if should brute force all outputs to ensure its correct
+bruteForce = len(sys.argv) > 3
+if (bruteForce):
+    print("Will brute force all coords --> int conversions as well. Expect long run time")
+# Find nearest integer point from coordinates given by checking all of them
+def nearest_point_brute_force(target):
+    global Points
+    shortestDistance = float('inf')
+    toReturn = 0
+    for i in range(len(Points)):
+        distance = math.sqrt(((float(Points[i][0])-float(target[0]))**2) + ((float(Points[i][1])-float(target[1]))**2))
+        if (distance < shortestDistance):
+            shortestDistance = distance
+            toReturn = i
+    return toReturn
+
 
 # Find nearest integer point from coordinates given
 def nearest_point(target):
@@ -27,6 +45,11 @@ tree = KDTree(Points)
 with open(fileToWriteTo, 'w') as fileWrite:
     pass
 
+# print(nearest_point((-73.67644, 42.73037)))
+# print(nearest_point_brute_force((-73.67644, 42.73037)))
+# print(Points[nearest_point((-73.67644, 42.73037))])
+# exit()
+
 #Read all data points from first command line argument
 with open(fileToReadFrom, 'r') as fileRead:
     for line in fileRead:
@@ -48,6 +71,10 @@ with open(fileToReadFrom, 'r') as fileRead:
                     splitUp[i] = ""
                 if (found == 2):
                     splitUp[i] = nearest_point((doubleCoords[0], doubleCoords[1]))
+                    if (bruteForce):
+                        responce = nearest_point_brute_force((doubleCoords[0], doubleCoords[1]))
+                        if (splitUp[i] != responce):
+                            print("ERROR. DIFFERENT RESPONCES!!!")
 
         #Write to output file
         with open(fileToWriteTo, 'a') as fileWrite:
@@ -57,9 +84,3 @@ with open(fileToReadFrom, 'r') as fileRead:
                     item = str(item).replace("\n", "")
                     stringOfLine += str(item) + ","
             fileWrite.write(stringOfLine[0:-1]+"\n")
-        
-
-        
-
-# print(nearest_point((-73.67644, 42.73037)))
-# print(Points[nearest_point((-73.67644, 42.73037))])
